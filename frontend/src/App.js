e4fb34a27d886ccf95018936e825a570b2f401c8
Asheeque  Chericham Veettil Maliyakkal <asheeque.cm@gmail.com>
2023-07-14 16:21:16 -0400

    frontend initial done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,20 @@
-
-const App = () =>{
-
-
-  return(
-    <h2>SHOP</h2>
-  )
-}
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer.component";
+import Header from "./components/Header.component";
+import HomeScreen from "./screens/HomeScreen";
+const App = () => {
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
 export default App;
